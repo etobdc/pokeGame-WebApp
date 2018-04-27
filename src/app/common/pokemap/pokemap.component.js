06d05fb3c4be0ee4e151a72baa40b5c9c1196445
Everton Braatz Dalla Corte <etodalla@gmail.com>
2018-04-26 22:03:59 -0300

    feat(media): adiciona novos tipos aos blocos, adiciona imagens aos tipos

--- src/app/common/pokemap/pokemap.component.js
+++ src/app/common/pokemap/pokemap.component.js
@@ -15,24 +15,73 @@ export const PokemapComponent = {
       this.imageType = [
         {
           type: 1,
-          name: 'Grow Grass',
-          image: '../../../../media/growGrass.png'
+          name: 'Grass 1',
+          image: '../../../../media/grass_1.png'
         },
         {
           type: 2,
-          name: 'Grass',
-          image: '../../../../media/grass.png'
+          name: 'Grass 2',
+          image: '../../../../media/grass_2.png'
         },
         {
           type: 3,
-          name: 'Road 1',
-          image: '../../../../media/road1.png'
+          name: 'Grass 3',
+          image: '../../../../media/grass_3.png'
         },
         {
           type: 4,
+          name: 'Grass Flowers 1',
+          image: '../../../../media/grass_flowers_1.png'
+        },
+        {
+          type: 5,
+          name: 'Ground 1',
+          image: '../../../../media/ground_1.png'
+        },
+        {
+          type: 6,
+          name: 'Road 1',
+          image: '../../../../media/road_1.png'
+        },
+        {
+          type: 7,
           name: 'Road 2',
-          image: '../../../../media/road2.png'
-        }
+          image: '../../../../media/road_2.png'
+        },{
+          type: 8,
+          name: 'Ground 1',
+          image: '../../../../media/rock_1.png'
+        },
+        {
+          type: 9,
+          name: 'Tall Grass 1',
+          image: '../../../../media/tall_grass_1.png'
+        },
+        {
+          type: 10,
+          name: 'Tall Grass 2',
+          image: '../../../../media/tall_grass_2.png'
+        },
+        {
+          type: 11,
+          name: 'Water 1',
+          image: '../../../../media/water_1.png'
+        },
+        {
+          type: 12,
+          name: 'Water 2',
+          image: '../../../../media/water_2.png'
+        },
+        {
+          type: 13,
+          name: 'Wood Fance',
+          image: '../../../../media/wood_face_1.png'
+        },
+        {
+          type: 14,
+          name: 'Flowers 1',
+          image: '../../../../media/flower_1.png'
+        },
       ]
     }
 
