600e6b38db1a577104169491dbea84fc53ac9f6a
Everton Braatz Dalla Corte <etodalla@gmail.com>
2018-04-28 12:24:15 -0300

    feat(pokemap): cria lógica de objetos para serem utiizados sobre o tipo de terreno, objetos js modificados para atender nova lógica

--- src/app/common/pokemap/pokemap.component.js
+++ src/app/common/pokemap/pokemap.component.js
@@ -14,73 +14,139 @@ export const PokemapComponent = {
       this.press = false;
       this.imageType = [
         {
-          type: 1,
+          type: 0,
           name: 'Grass 1',
-          image: '../../../../media/grass_1.png'
+          image: '../../../../media/grass_1.png',
+          canPass: true,
+          canHavePokemon:0,
+          canHaveObject:true,
+          isObject:false,
+        },
+        {
+          type: 1,
+          name: 'Grass 2',
+          image: '../../../../media/grass_2.png',
+          canPass: true,
+          canHavePokemon:0,
+          canHaveObject:true,
+          isObject:false,
         },
         {
           type: 2,
-          name: 'Grass 2',
-          image: '../../../../media/grass_2.png'
+          name: 'Grass 3',
+          image: '../../../../media/grass_3.png',
+          canPass: true,
+          canHavePokemon:0,
+          canHaveObject:true,
+          isObject:false,
         },
         {
           type: 3,
-          name: 'Grass 3',
-          image: '../../../../media/grass_3.png'
+          name: 'Grass Flowers 1',
+          image: '../../../../media/grass_flowers_1.png',
+          canPass: true,
+          canHavePokemon:0,
+          canHaveObject:true,
+          isObject:false,
         },
         {
           type: 4,
-          name: 'Grass Flowers 1',
-          image: '../../../../media/grass_flowers_1.png'
+          name: 'Ground 1',
+          image: '../../../../media/ground_1.png',
+          canPass: true,
+          canHavePokemon:0,
+          canHaveObject:true,
+          isObject:false,
         },
         {
           type: 5,
-          name: 'Ground 1',
-          image: '../../../../media/ground_1.png'
+          name: 'Road 1',
+          image: '../../../../media/road_1.png',
+          canPass: true,
+          canHavePokemon:0,
+          canHaveObject:true,
+          isObject:false,
         },
         {
           type: 6,
-          name: 'Road 1',
-          image: '../../../../media/road_1.png'
+          name: 'Road 2',
+          image: '../../../../media/road_2.png',
+          canPass: true,
+          canHavePokemon:0,
+          canHaveObject:true,
+          isObject:false,
         },
         {
           type: 7,
-          name: 'Road 2',
-          image: '../../../../media/road_2.png'
-        },{
+          name: 'Tall Grass 1',
+          image: '../../../../media/tall_grass_1.png',
+          canPass: true,
+          canHavePokemon:0.4,
+          canHaveObject:false,
+          isObject:false,
+        },
+        {
           type: 8,
-          name: 'Ground 1',
-          image: '../../../../media/rock_1.png'
+          name: 'Tall Grass 2',
+          image: '../../../../media/tall_grass_2.png',
+          canPass: true,
+          canHavePokemon:0.4,
+          canHaveObject:false,
+          isObject:false,
         },
         {
           type: 9,
-          name: 'Tall Grass 1',
-          image: '../../../../media/tall_grass_1.png'
+          name: 'Water 1',
+          image: '../../../../media/water_1.png',
+          canPass: false,
+          canHavePokemon:0.4,
+          canHaveObject:false,
+          isObject:false,
         },
         {
           type: 10,
-          name: 'Tall Grass 2',
-          image: '../../../../media/tall_grass_2.png'
+          name: 'Water 2',
+          image: '../../../../media/water_2.png',
+          canPass: false,
+          canHavePokemon:0.4,
+          canHaveObject:false,
+          isObject:false,
         },
         {
           type: 11,
-          name: 'Water 1',
-          image: '../../../../media/water_1.png'
+          name: 'Wood Fance',
+          image: '../../../../media/wood_face_1.png',
+          canPass: false,
+          canHavePokemon:0,
+          canHaveObject:false,
+          isObject:true,
         },
         {
           type: 12,
-          name: 'Water 2',
-          image: '../../../../media/water_2.png'
+          name: 'Flowers 1',
+          image: '../../../../media/flower_1.png',
+          canPass: true,
+          canHavePokemon:0,
+          canHaveObject:false,
+          isObject:false,
         },
         {
           type: 13,
-          name: 'Wood Fance',
-          image: '../../../../media/wood_face_1.png'
+          name: 'Tree 1',
+          image: '../../../../media/arvore_1.png',
+          canPass: false,
+          canHavePokemon:0,
+          canHaveObject:false,
+          isObject:true,
         },
         {
           type: 14,
-          name: 'Flowers 1',
-          image: '../../../../media/flower_1.png'
+          name: 'Rock 1',
+          image: '../../../../media/rock_1.png',
+          canPass: false,
+          canHavePokemon:0,
+          canHaveObject:false,
+          isObject:true,
         },
       ]
     }
@@ -99,13 +165,30 @@ export const PokemapComponent = {
       for (var i = 0; i < this.totalMapa; i++) {
         let linhas = [];
         for (var a = 0; a < this.totalMapa; a++) {
-          linhas.push({id:a, id_map:i, type: this.type, hasPoekemon: false});
+          linhas.push({
+            id:a,
+            id_map:i,
+            type: this.type,
+            object:''
+          });
         }
         this.mapa.push({id:i, colunas:linhas});
       }
     }
     alteraTipo(map,col){
-      this.mapa[map].colunas[col].type = this.type;
+      if(!this.imageType[this.type].isObject){
+        if(this.mapa[map].colunas[col].object != ''){
+          if(this.imageType[this.type].canHaveObject){
+            this.mapa[map].colunas[col].type = this.type;
+          }
+        }else{
+          this.mapa[map].colunas[col].type = this.type;
+        }
+      }else{
+        if(this.imageType[this.mapa[map].colunas[col].type].canHaveObject){
+          this.mapa[map].colunas[col].object = this.type;
+        }
+      }
     }
     handleMouseover(map,col){
       if(this.press){
